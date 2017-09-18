bdeb1f76e4954b46162dc51de2db2e651f11f291
Darragh McGee <darraghmcgee20@gmail.com>
2017-09-18 21:34:04 +0200

    klimttd: update vendor path in lineage.mk
    Change-Id: I3faae4ba10d0365267ff97023cb13fcbb94cfd1b
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -8,7 +8,7 @@ $(call inherit-product, device/samsung/klimttd/device.mk)
 PRODUCT_BRAND := samsung
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit more LineageOS stuff.
 $(call inherit-product, vendor/cm/config/telephony.mk)
