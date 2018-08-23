ba8fd36e7514d1011cb27a59c212b76539eba5fa
Martin Bouchet <tincho5588@gmail.com>
2018-08-23 22:05:44 +0000

    klimttd: use a wrapper to update the sensors HAL to 1.3
    Current prebuilt sensors hal is version 1.0. From Android 8.0 and
    onwards the minimun version is 1.3.
    Let's wrap the prebuilt sensors hal to get it from 1.0 to 1.3.
    
    Change-Id: Ie2772f39e15ce7e3010bbbd011488cf169da5565
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown
+# Sensors wrapper
+PRODUCT_PACKAGES += \
+    sensors.universal5420
 
 # call dalvik heap and hwui config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
